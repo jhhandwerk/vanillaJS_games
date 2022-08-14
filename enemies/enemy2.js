0390f9cbd2cf2eaa0e41b337ccc13de76f07052f
Jonathan Handwerk <jonathanhandwerk@Jonathans-MacBook-Air.local>
2022-08-14 09:07:57 +0900

    added a bunch of tuff. sorry for not pushing. recently, just added a collison detection piece.

--- enemies/enemy2.js
+++ enemies/enemy2.js
@@ -11,20 +11,26 @@ let gameFrame = 0;
 class Enemy {
     constructor(){
         this.Image = new Image();
-        this.Image.src = "./enemiesIMG/enemy1.png";
-        // this.speed = Math.random() * 4 -2;
-        this.spriteWidth = 293;
-        this.spriteHeight = 155;
+        this.Image.src = "./enemiesIMG/enemy2.png";
+        this.speed = Math.random() * 4 + 1;
+        this.spriteWidth = 266;
+        this.spriteHeight = 188;
         this.width = this.spriteWidth/3;
         this.height = this.spriteHeight;
         this.x = Math.random() * (canvas.width - this.width);
         this.y = Math.random() * (canvas.height - this.height);
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random()*3+1);
+        // this.angle = Math.random() * 2;
+        this.angle = 0;
+        this.angleSpeed = Math.random() * 0.2;
+        this.curve = Math.random() * 7;
     }
     update(){
-        this.x += Math.random() * 5 -2.5;
-        this.y += Math.random() * 5 -2.5;
+        this.x -= this.speed;
+        this.y += this.curve * Math.sin(this.angle);
+        this.angle += this.angleSpeed;
+        if(this.x + this.width < 0) this.x = canvas.width;
         // animate sprites
         if (gameFrame % this.flapSpeed === 0){
             this.frame > 4 ? this.frame = 0 : this.frame++;
